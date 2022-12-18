f7c49faadf0eee65728c578aec7e1c3b05a4316a
Krunal <krunal@Krunals-MacBook-Pro.local>
2022-12-18 18:18:18 -0500

    initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import { UiComponents } from '@nx-react/ui-components';
+
+export function App() {
+  return (
+    <>
+      <UiComponents />
+      <div />
+    </>
+  );
+}
+
+export default App;
