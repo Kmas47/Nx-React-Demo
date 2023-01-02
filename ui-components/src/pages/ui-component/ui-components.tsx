02fdd08b0067eb632249f917e5c65360ca3b6eeb
Krunal <krunal@Krunals-MBP.hitronhub.home>
2023-01-02 14:55:38 -0500

    fix: test redirection

--- ui-components/src/pages/ui-component/ui-components.tsx
+++ ui-components/src/pages/ui-component/ui-components.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './ui-components.module.scss';
 
 export default function UiComponents() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to UiComponents!</h1>
+      <div>
+        <h1>Welcome to UiComponents!</h1>
+      </div>
+      <div>
+        <Link to={'/random-error-page'}>Redirect to error page</Link>
+      </div>
     </div>
   );
 }
