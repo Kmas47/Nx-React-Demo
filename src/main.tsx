953a71dff7cd060f18e1553c8a88987dcf594bde
Krunal <krunal@Krunals-MacBook-Pro.local>
2024-08-18 20:02:30 -0400

    feat: add speed-insights from vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import App from './app/app';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>
 );
