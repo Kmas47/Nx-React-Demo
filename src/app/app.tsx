bde6d3ba6bf0cfd421aa46a1c164bf6c06b474fd
Krunal <krunal@Krunals-MacBook-Pro.local>
2024-10-25 23:14:58 -0400

    feat: lazy load third party

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { RouterProvider } from 'react-router-dom';
 import styles from './app.module.scss';
-import { Analytics } from '@vercel/analytics/react';
 import { HelmetProvider } from 'react-helmet-async';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 import { router } from './navigation/router';
+import React, { Suspense } from 'react';
+
+const Analytics = React.lazy(() =>
+  import('@vercel/analytics/react').then((mod) => ({ default: mod.Analytics }))
+);
+
+const SpeedInsights = React.lazy(() =>
+  import('@vercel/speed-insights/react').then((mod) => ({
+    default: mod.SpeedInsights,
+  }))
+);
 
 export function App() {
   return (
     <>
       <HelmetProvider>
-        <Analytics />
-        <SpeedInsights />
+        <Suspense fallback={null}>
+          <Analytics />
+          <SpeedInsights />
+        </Suspense>
         <RouterProvider router={router} />
       </HelmetProvider>
     </>
